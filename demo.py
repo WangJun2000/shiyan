46851f93c75775dce43f22f7c4b22d750ac4c7e7
WangJun2000 <2595676015@qq.com>
2020-10-02 21:48:04 +0800

    第二次提交

--- demo.py
+++ demo.py
@@ -3,6 +3,6 @@ import string
 #os.rename("F:\大二上课程\智能硬件应用实验\音乐列表\South of the Border (feat. Camila Cabello & Cardi B).m4a","F:\大二上课程\智能硬件应用实验\音乐列表\SouthoftheBorder(feat.CamilaCabello&CardiB).m4a")
 '''i = 'hello world i am li'
 i1=i.translate(str.maketrans('hello','i7777','')).translate(str.maketrans('','','7777'))
-print(i1)'''
-#os.system("ffmpeg -i " + "G:\CloudMusic\VipSongsDownload\TaylorSwiftLoveStory.qmcflac"
-#                         + " " + "G:\CloudMusic\VipSongsDownload\1.mp3")
+print(i1)'''     
+  #os.system("ffmpeg -i " + "G:\CloudMusic\VipSongsDownload\TaylorSwiftLoveStory.qmcflac"
+  #                         + " " + "G:\CloudMusic\VipSongsDownload\1.mp3")
